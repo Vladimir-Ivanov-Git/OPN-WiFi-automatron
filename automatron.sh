1e207c60e3b53e85497d03b1c3ced60add889718
Ivanov Vladimir <ivanov.vladimir.mail@gmail.com>
2018-05-27 19:13:37 +0300

    Change output and add watchdog for hostapd and dnsmasq.

--- automatron.sh
+++ automatron.sh
@@ -13,6 +13,17 @@ DHCP_MASK="255.255.255.0"
 RESOLVE_SERVER_1="1.1.1.1"
 RESOLVE_SERVER_2="1.0.0.1"
 
+BLUE='\033[1;34m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+ORANGE='\033[1;33m'
+NC='\033[0m'
+
+INFO="${BLUE}[*]${NC} "
+ERROR="${RED}[-]${NC} "
+SUCESS="${GREEN}[+]${NC} "
+WARNING="${ORANGE}[!]${NC} "
+
 # Kill interrupt process
 /etc/init.d/network-manager stop 2>/dev/null
 killall wpa_supplicant 2>/dev/null
@@ -21,23 +32,34 @@ killall wpa_supplicant 2>/dev/null
 while true
 do
 	if ip link show | grep "${INTERNET_IFACE}" >/dev/null 2>&1; then
-		echo "Interface ${INTERNET_IFACE} is up.";
+		echo -e "${SUCESS}Interface ${INTERNET_IFACE} exist!";
 		break
 	else
-		echo "Interface ${INTERNET_IFACE} is down, wait..."
-		sleep 5
+		echo -e "${ERROR}Interface ${INTERNET_IFACE} not found, wait..."
+		sleep 10
 fi
 done
 
 while true
 do
-	dhclient ${INTERNET_IFACE}
-        if ifconfig ${INTERNET_IFACE} | grep "inet " >/dev/null 2>&1; then
-                echo "Interface ${INTERNET_IFACE} is configured.";
-                break
-        else
-                echo "Interface ${INTERNET_IFACE} is not configured, wait..."
-                sleep 30
+	if ip link show | grep "${AP_IFACE}" >/dev/null 2>&1; then
+		echo -e "${SUCESS}Interface ${AP_IFACE} exist!";
+		break
+	else
+		echo -e "${ERROR}Interface ${AP_IFACE} not found, wait..."
+		sleep 10
+fi
+done
+
+while true
+do
+	dhclient ${INTERNET_IFACE} >/dev/null 2>&1
+    if ifconfig ${INTERNET_IFACE} | grep "inet " >/dev/null 2>&1; then
+        echo -e "${SUCESS}Interface ${INTERNET_IFACE} is configured.";
+        break
+    else
+        echo -e "${SUCESS}Interface ${INTERNET_IFACE} is not configured, wait..."
+        sleep 10
 fi
 done
 
@@ -46,20 +68,27 @@ ifconfig ${AP_IFACE} ${AP_IP}/${AP_MASK}
 ifconfig ${AP_IFACE} up
 
 # Check installed programs
-if ! dpkg --list | grep -qe "ii\s*apache2 "; then apt -y install apache2; fi
-if ! dpkg --list | grep -qe "ii\s*libapache2-mod-security2 "; then apt -y install libapache2-mod-security2; fi
-if ! dpkg --list | grep -qe "ii\s*hostapd "; then apt -y install hostapd; fi
-if ! dpkg --list | grep -qe "ii\s*dnsmasq "; then apt -y install dnsmasq; fi
+if ! dpkg --list | grep -qe "ii\s*apache2 "; then apt -y -qq install apache2; fi
+if ! dpkg --list | grep -qe "ii\s*libapache2-mod-security2 "; then apt -y -qq install libapache2-mod-security2; fi
+if ! dpkg --list | grep -qe "ii\s*hostapd "; then apt -y -qq install hostapd; fi
+if ! dpkg --list | grep -qe "ii\s*dnsmasq "; then apt -y -qq install dnsmasq; fi
 
 # IP forward
 iptables -t nat -F
+iptables -t nat -X
 iptables -F
+iptables -X
+
 iptables -t nat -A POSTROUTING -o ${INTERNET_IFACE} -j MASQUERADE
 iptables -A FORWARD -i ${AP_IFACE} -o ${INTERNET_IFACE} -j ACCEPT
 echo '1' > /proc/sys/net/ipv4/ip_forward
 
 # Apache2 fish settings
-if [ -f ${CURRENT_DIR}/hosts.txt ]; then
+if [ ! -d "${CURRENT_DIR}/Apache2-fish/" ]; then
+    git clone https://github.com/Vladimir-Ivanov-Git/Apache2-fish.git ${CURRENT_DIR}/Apache2-fish/
+fi
+
+if [ -f "${CURRENT_DIR}/hosts.txt" ]; then
 	rm -f ${CURRENT_DIR}/hosts.txt
 fi
 
@@ -84,8 +113,18 @@ ssid=${AP_ESSID}
 channel=${AP_CHANELL}
 EOH
 
+chmod 666 ${CURRENT_DIR}/hostapd.conf
 kill -9 $(pgrep hostapd) 2>/dev/null
-/usr/sbin/hostapd ${CURRENT_DIR}/hostapd.conf -B -f /var/log/hostapd.log
+
+while true
+do
+    if pgrep -x "hostapd" > /dev/null
+    then
+        sleep 10
+    else
+        /usr/sbin/hostapd ${CURRENT_DIR}/hostapd.conf -B -f /var/log/hostapd.log
+    fi
+done &
 
 # Dnsmasq settings
 cat >${CURRENT_DIR}/dnsmasq.conf <<EOD
@@ -99,5 +138,24 @@ log-queries
 log-facility=/var/log/dnsmasq.log
 EOD
 
+echo -n "address=/" >> ${CURRENT_DIR}/dnsmasq.conf
+while read -r domain
+do
+        echo -n "${domain}/" >> ${CURRENT_DIR}/dnsmasq.conf
+done < ${CURRENT_DIR}/hosts.txt
+
+echo "${AP_IP}" >> ${CURRENT_DIR}/dnsmasq.conf
+rm ${CURRENT_DIR}/hosts.txt
+
+chmod 666 ${CURRENT_DIR}/dnsmasq.conf
 kill -9 $(pgrep dnsmasq) 2>/dev/null
-/usr/sbin/dnsmasq -C ${CURRENT_DIR}/dnsmasq.conf -H ${CURRENT_DIR}/hosts.txt -d >/var/log/dnsmasq_debug.log 2>&1 &
+
+while true
+do
+    if pgrep -x "dnsmasq" > /dev/null
+    then
+        sleep 10
+    else
+        /usr/sbin/dnsmasq -C ${CURRENT_DIR}/dnsmasq.conf
+    fi
+done &
