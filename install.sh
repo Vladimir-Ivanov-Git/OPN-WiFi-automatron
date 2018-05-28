a630cd383da25d439c3ee4471ff81a48c86a1b96
Ivanov Vladimir <ivanov.vladimir.mail@gmail.com>
2018-05-28 23:53:51 +0300

    Add install script.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_NAME="open-wifi-automatron"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INIT_DIR="/etc/init.d"
+
+cp ${CURRENT_DIR}/${SCRIPT_NAME} ${INIT_DIR}/
+chmod +x ${INIT_DIR}/${SCRIPT_NAME}
+
+${INIT_DIR}/${SCRIPT_NAME} init
+systemctl enable ${SCRIPT_NAME}.service
